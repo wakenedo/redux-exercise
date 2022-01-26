04ed35f462f4ed8661a632e77ee5a6aa4d44de22
wakenedo <a_nuner@outlook.com>
2022-01-26 10:15:31 -0300

    Part 1 - Reducers, Actions, Axios & JSONserver
    App:
    installed Axios and JSONserver to mock an api to better understand Redux states in a realistic environment.
    
    +services Folder
    +server.js File
    
    services:
    Set up axios baseURL with axios.create
    +api.ts
    
    server.ts:
    File with the fake api data, to be used as an api with JSONserver
    
    store:
    modules:
    +rootReducer
    rootReducer: Setup a file to combine reducers for different actions in the application
    
    cart:
    +actions.ts
    +reducer.ts
    +types.ts
    
    actions.ts:
    setup actions.ts with ADD_PRODUCT_TO_CART action with addProductToCart function adding a product as a payload when called
    
    reducer.ts:
    Setup a file to receive and update states related to the cart module
    types.ts:
    Created an file to handle all types for the cart module

---
+++ src/store/modules/cart/reducer.ts
@@ -0,0 +1,14 @@
+import {Reducer} from 'redux'
+import { ICartState } from './types';
+
+const INITIAL_STATE: ICartState = {
+    items: []
+};
+
+const cart: Reducer<ICartState> = (state, action) => {
+    console.log(state, action)
+
+    return INITIAL_STATE;
+}
+
+export default cart; 
---
+++ src/store/modules/cart/types.ts
@@ -0,0 +1,13 @@
+export interface IProduct {
+    id: number;
+    title: string;
+    price: number;
+}
+export interface ICartItem {
+    product: object;
+    quantity: number;
+}
+
+export interface ICartState {
+    items: ICartItem[];
+}
